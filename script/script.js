2333c3fbf2df2f62a7ab0da044557cea70fe6162
Sahitya hp <neupanesahitya1@gmail.com>
2024-04-18 10:35:44 -0400

    got the searched result in console while doing fetch

--- script/script.js
+++ script/script.js
@@ -1,16 +1,80 @@
 // my api key, although it's not good idea to share api key but I am doing this only for the assignment
 const apiKey = `e0a94e96a35ab53298c6e02bbfce980b`;
-let city;
 
 // this is end point for the api to get the city name
-const cityUrlEndpoint = `https://api.openweathermap.org/geo/1.0/direct?q=Barrie&limit=5&appid=${apiKey}`;
+let cityData;
+let searchCity = "barrie";
+let cityArray = [];
+let cityUrlEndpoint = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${apiKey}`;
 
+// this is end point for the getting the weather data
 let latitude;
 let longitude;
-// this is end point for the getting the weather data
+let weatherData;
 const weatherUrlEndpoint = ` https://api.openweathermap.org/data/2.5/weather?lat=44.3893113&lon=-79.6901736&appid=${apiKey}`;
 
-const weatherData = async () => {
+// targeting the html elements
+const searchBox = document.querySelector(".searchBox");
+
+// creating a function that will get us the data while fetching the url
+const getData = async (endpointUrl) => {
+  try {
+    const response = await fetch(`${endpointUrl}`);
+
+    let data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+  // data = await fetch(`${endpointUrl}`)
+  //   .then((response) => {
+  //     // console.log(response.json);
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // console.log(data);
+  // return data;
+};
+
+// adding event listener while doing on change
+
+searchBox.addEventListener("keyup", async (e) => {
+  // each time the above event listener is triggered the array should be empty
+  cityArray = [];
+  searchCity = e.target.value;
+  // only the api will be fetched if the search length is greater than 0
+  if (searchCity.length > 0) {
+    // giving search value to search url
+    cityUrlEndpoint = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=6&appid=${apiKey}`;
+
+    // i'll get the searched city name over here
+    cityData = await getData(cityUrlEndpoint);
+    console.log(cityData);
+    // filtering the text so that we only get the data that is filtered
+    const filterCityData = cityData.filter((item) => {
+      return item.name.toLowerCase().startsWith(searchCity.toLowerCase());
+    });
+
+    // looping the cityData that I got from searched value
+    filterCityData.forEach((e) => {
+      // making another array just with cityname and country to display
+      cityArray.push(`${e.name}, ${e.country}`);
+    });
+    console.log(cityArray);
+    // console.log(data.json);
+  }
+  // getData(data)
+});
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+const getWeatherData = async () => {
   const data = await fetch(
     `https://api.openweathermap.org/geo/1.0/direct?q=london&limit=10&appid=${apiKey}`,
     {
